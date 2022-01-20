92faf5eb4a5ceca9e0139812a8daa1f7f5f8338c
AbdulAziz0682 <abdulrasheed2132@gmail.com>
2022-01-20 19:19:03 +0500

    Added Executive page

---
+++ src/components/executives/Executive.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styles from './Executive.module.css';
+
+import AwaisShaikh from '../../assets/awais_shaikh.jpg';
+import redArrow from '../../assets/red-arrow.svg';
+
+function Executive() {
+  return (
+    <div className="page-container">
+      <div className="page-content">
+        <div className="flex flex-wrap w-full">
+          <div className={styles['profile-container']}>
+            <div className={styles['profile-details']}>
+              <div className="flex w-full gap-3">
+                <img src={redArrow} alt="red arrow" className="w-4 h-4 transform rotate-180" />
+                <span className="text-xs">Back to Executives</span>
+              </div>
+              <h1 className={styles['name-heading']}>Awais Shaikh</h1>
+              <h6 className={styles['name-sub-heading']}>Chief Executive Officer</h6>
+              <p className="w-full mt-12">
+                Head of the Pack. Mustafa is serving as Chairman of the Board and CEO
+                of Renesis Tech. Mustafa leads Renesis Tech&apos;s strategic development.
+                Along with Renesis Tech, Mustafa is also a Board member of Arcadian
+                Lab, which focuses on mobile game development working with some of the
+                biggest publishers in the world.
+              </p>
+              <p className="mt-6 text-gray-400">Mustafa received his Bachelors from Western University</p>
+            </div>
+            <div className={styles.navigator}>
+              <div>
+                <p className="text-lg font-bold">Usman Rana</p>
+                <p className="flex gap-3 text-xs uppercase">
+                  <img src={redArrow} alt="red arrow" className="w-4 h-4 transform rotate-180" />
+                  <span>Operations Manager</span>
+                </p>
+              </div>
+              <div>
+                <p className="text-lg font-bold">Abdul Wadood</p>
+                <p className="flex justify-end gap-3 text-xs uppercase">
+                  <span>Designer</span>
+                  <img src={redArrow} alt="red arrow" className="w-4 h-4" />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles['image-container']}>
+            <img src={AwaisShaikh} alt="ceo" className="object-contain w-full h-full" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Executive;
--- src/components/executives/index.jsx
+++ src/components/executives/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { Link, Routes, Route } from 'react-router-dom';
 
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
@@ -8,6 +8,7 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import styles from './Executives.module.css';
 
 import AwaisShaikh from '../../assets/awais_shaikh.jpg';
+import Executive from './Executive';
 
 const handleDragStart = (e) => e.preventDefault();
 
@@ -30,28 +31,36 @@ function Executives() {
     </Link>
   ));
   return (
-    <div className="page-container">
-      <div className="md:px-0 page-content">
-        <AliceCarousel
-          autoPlay
-          infinite
-          animationEasingFunction="ease"
-          disableDotsControls
-          disableButtonsControls
-          autoPlayInterval="2000"
-          animationDuration="1000"
-          mouseTracking
-          items={list}
-          responsive={{
-            0: { items: 1 },
-            600: { items: 2 },
-            900: { items: 3 },
-            1200: { items: 4 },
-            1500: { items: 6 },
-          }}
-        />
-      </div>
-    </div>
+    <Routes>
+      <Route
+        index
+        element={(
+          <div className="page-container">
+            <div className="md:px-0 page-content">
+              <AliceCarousel
+                autoPlay
+                infinite
+                animationEasingFunction="ease"
+                disableDotsControls
+                disableButtonsControls
+                autoPlayInterval="2000"
+                animationDuration="1000"
+                mouseTracking
+                items={list}
+                responsive={{
+                  0: { items: 1 },
+                  600: { items: 2 },
+                  900: { items: 3 },
+                  1200: { items: 4 },
+                  1500: { items: 6 },
+                }}
+              />
+            </div>
+          </div>
+        )}
+      />
+      <Route path="/:name" element={<Executive />} />
+    </Routes>
   );
 }
 
